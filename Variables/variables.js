04d5c7045c5ba395ded9a15e272d29405c86f395
Remy Meier <remad@web.de>
2023-09-19 21:23:01 +0200

    master: number variables initilazation

---
+++ Variables/variables.js
@@ -0,0 +1,6 @@
+var n1 = 10;
+console.log(n1);
+var s1 = "You are the creator of your destiny";
+var s2 = "all the power is with in you, you can do anything and everything.";
+console.log(s1);
+console.log(s2);
---
+++ variables.ts
@@ -0,0 +1,7 @@
+var n1:number =  10;
+console.log(n1);
+
+var s1:string = "You are the creator of your destiny";
+var s2:string = `all the power is with in you, you can do anything and everything.`;
+console.log(s1);
+console.log(s2);
