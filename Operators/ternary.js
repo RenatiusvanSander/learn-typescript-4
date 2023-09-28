651ae657046d42d08dd695f875424ba41f98b1d0
Remy Meier <remad@web.de>
2023-09-28 21:30:15 +0200

    master: one ternary operator example

---
+++ Operators/ternary.js
@@ -0,0 +1,4 @@
+var x = 20;
+var y = 10;
+console.log((x > y) ? "X is greater than Y" : "Y is greater than x");
+console.log((x < y) ? "X is greater than Y" : "Y is greater than x");
---
+++ Operators/ternary.ts
@@ -0,0 +1,5 @@
+var x:number = 20;
+var y:number = 10;
+
+console.log((x>y) ? "X is greater than Y" : "Y is greater than x");
+console.log((x<y) ? "X is greater than Y" : "Y is greater than x");
