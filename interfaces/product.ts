9a10965d8bd9486f7e560d4ae446da5ba69e801b
RenatiusvanSander <remad@web.de>
2023-11-07 17:18:29 +0100

    master: interface Porduct in typescript

---
+++ interfaces/product.ts
@@ -0,0 +1,13 @@
+interface Product {
+    id:number;
+    name:string;
+    description:string;
+    price:number;
+}
+
+var product1:Product = {
+    id:123,
+    name:"Remy",
+    description:"Remy product",
+    price:12.56;
+}
