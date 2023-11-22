dfaa7c3de6e477c0a13fdf3d9104af371b68edc9
Remy Meier <remad@web.de>
2023-11-22 14:55:48 +0100

    master: polymorphism in action in typescript

--- classes/static.ts
+++ classes/static.ts
@@ -3,10 +3,16 @@ class Check {
     customerName:string;
     accNo:number;
     routingNo:number;
+
+    display() {
+        Check.bankName = "BOA";
+        console.log(Check.bankName);
+    }
 }
 
 var check =new Check();
 Check.bankName = "BOA";
+check.display();
 
 var check2 = new Check();
 check2.accNo;
