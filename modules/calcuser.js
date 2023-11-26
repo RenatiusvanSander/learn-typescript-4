42ae43f62247e3d42ac2ed466a94a4568259b031
Remy Meier <remad@web.de>
2023-11-26 16:41:00 +0100

    master: import with alias in typescript

---
+++ modules/calcuser.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var calc = require("./calc");
+console.log(calc.add(2, 3));
+console.log(calc.sub(10, 5));
---
+++ modules/calcuser.ts
@@ -0,0 +1,3 @@
+import * as calc from './calc';
+console.log(calc.add(2,3));
+console.log(calc.sub(10,5));
