f489078ec45474f6a929945762b19644a5162e95
Remy Meier <remad@web.de>
2023-11-26 17:55:45 +0100

    master: maps in typescript

---
+++ map/maptype.ts
@@ -0,0 +1,6 @@
+let studentScores = new Map([["john",90],["Pamela",121], ["Tobias",862]]);
+console.log(studentScores.get("john"));
+studentScores.set("Remy", 200);
+console.log(studentScores.get("Remy"));
+studentScores.delete("remy");
+console.log(studentScores.has("Remy"));
